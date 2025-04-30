6b35c6af4610acc4c473c7ae82b2d39bcd3bf368
NimmyThankachan <nimmythankachanmt@gmail.com>
2025-04-30 14:47:49 +0530

    Commit

--- src/main/java/com/training/ui/BillController.java
+++ src/main/java/com/training/ui/BillController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 import com.training.model.Bill;
+import com.training.model.Customer;
 import com.training.service.BillService;
 
 @RestController
@@ -20,9 +23,14 @@ public class BillController {
 	@Autowired
 	BillService service;
 
+	@Autowired
+	RestTemplate template;
+
 	@PostMapping(value = "/add")
 	public ResponseEntity<Bill> f1(@RequestBody Bill bill) {
 		Bill bill1 = this.service.addNewBill(bill);
+		String url = "http://localhost:9121/api/showAll";
+		int result = template.getForObject(url, null, Customer.class);
 		return new ResponseEntity<>(bill1, HttpStatus.CREATED);
 	}
 
