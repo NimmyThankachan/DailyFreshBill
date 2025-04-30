6751b96b3865989957fa9cad4d7ad4d923cf7fd4
NimmyThankachan <nimmythankachanmt@gmail.com>
2025-04-30 14:34:01 +0530

    Commit

--- src/main/java/com/training/DailyFreshBillMicroServiceApplication.java
+++ src/main/java/com/training/DailyFreshBillMicroServiceApplication.java
@@ -2,12 +2,20 @@ package com.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DailyFreshBillMicroServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DailyFreshBillMicroServiceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate template() {
+		return new RestTemplateBuilder().build();
 	}
 
 }
